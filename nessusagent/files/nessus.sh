efa0af23a6680e435a480456720b61fe61d37e3a
Olaitan Adebowale <pianolinux@gmail.com>
2019-08-24 22:09:16 -0400

    removed key

--- nessusagent/files/nessus.sh
+++ nessusagent/files/nessus.sh
@@ -4,7 +4,7 @@ source /etc/environment
 
 if [ -f /opt/nessus_agent/sbin/nessuscli ]; then
 echo "[*-] registering agent..."
-  /opt/nessus_agent/sbin/nessuscli agent link --key=55e240e57e604ee101a70bae54171adfd3831266d762a5c88555e19f6fcfd350 --host=cloud.tenable.com --port=443 --groups='USB_AWS_Converge_Devices' 
+  /opt/nessus_agent/sbin/nessuscli agent link --key= --host=cloud.tenable.com --port=443 --groups='USB_AWS_Converge_Devices' 
   echo "[*-] systemctl restart nessusagent.service..."
   systemctl restart nessusagent.service
 else
